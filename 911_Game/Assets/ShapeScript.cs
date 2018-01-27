968aeda0d574ab9703441a11a6253e95fbc2a4ea
Tori Rossini <tori.rossini42@gmail.com>
2018-01-26 23:01:23 -0500

    ShapeScript and CallScript
    Added: ShapeScript and CallScript
    ShapeScript:
        public GameObject centerBlock;
        public GameObject[] blocks;
        public Vector2[] points;
    CallScript (Properties included):
        private GameObject shape;
        private int pointValue;
        private float callTimePassed;
        private float callTime;
        private float callExpireTimePassed;
        private float callExpireTime;
        private bool callTaken;
        private Vector2 incomingCallPos;
        public Player player;

---
+++ 911_Game/Assets/ShapeScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShapeScript : MonoBehaviour
+{
+    public GameObject centerBlock;
+    public GameObject[] blocks;
+    public Vector2[] points;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
